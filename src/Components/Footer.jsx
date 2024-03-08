fb58cbe34c3d8c149988ca5786a4db85279323d8
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2024-03-08 23:09:03 +0400

    h3

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import './CSS/Footer.css'
 const Footer = () => {
     return ( 
         <footer>
-            <p>Made By <a href="https://georgianprocode.netlify.app/" target="_blank" style={{textDecoration: 'none', color: 'white'}}>GeorgiaProCode</a></p>
+            <p>Made By <a href="https://georgianprocode.netlify.app/" target="_blank" style={{textDecoration: 'underline', color: 'white'}}>GeorgiaProCode</a></p>
         </footer>
      );
 }
